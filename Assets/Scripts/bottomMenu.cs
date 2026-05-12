3100ea918b0c8ba5090a30a75db374bc5c716ffa
TermiJP <javier.corchado.fer@gmail.com>
2026-05-12 22:02:17 +0200

    menucanvas

--- Assets/Scripts/bottomMenu.cs
+++ Assets/Scripts/bottomMenu.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-
 using UnityEngine.UI;
 using static UnityEngine.UI.Image;
+using UnityEngine.SceneManagement;
 
-public class bottomMenu : MonoBehaviour , IPointerEnterHandler
+public class bottomMenu : MonoBehaviour , IPointerDownHandler , IPointerEnterHandler, IPointerExitHandler
 {
     public Image selected;
+
+    public enum TipoBottom
+    {
+        Play,
+        Settings,
+        Multiplayer,
+        Exit
+    }
+
+    public TipoBottom estadoBottom;
 
     void Start()
     {
@@ -21,5 +31,29 @@ public class bottomMenu : MonoBehaviour , IPointerEnterHandler
     public void OnPointerExit(PointerEventData eventData)
     {
         selected.enabled = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        switch (estadoBottom)
+        {
+            case TipoBottom.Play:
+                SceneManager.LoadScene("Gameplay");
+                break;
+            case TipoBottom.Settings:
+                var settingsCanvas = GameObject.Find("SettingsCanvas").GetComponent<Canvas>();
+                settingsCanvas.enabled = true;  
+                break;
+            case TipoBottom.Multiplayer:
+                var lobbyCanvas= GameObject.Find("LobbyCanvas").GetComponent<Canvas>();
+                lobbyCanvas.enabled = true;
+                var menuCanvas= GameObject.Find("Menu").GetComponent<Canvas>();
+                menuCanvas.enabled = false;
+                break;
+            case TipoBottom.Exit:
+                Application.Quit();
+                break;
+        }
+
     }
 }
