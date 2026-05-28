571ad94fa5effa4818ee07492933cb35b4247392
TermiJP <javier.corchado.fer@gmail.com>
2026-05-28 21:49:24 +0200

    ES MULTIJUGADOR

---
+++ Assets/Scripts/Multijugador/MainManuUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public RelayLobbyManager manager;
+
+    public TMP_InputField codeInput;
+
+    // BOTÓN HOST
+    public async void HostGame()
+    {
+        string lobbyCode = await manager.CreateLobby();
+
+        Debug.Log("Código lobby: " + lobbyCode);
+    }
+
+    // BOTÓN JOIN
+    public async void JoinGame()
+    {
+        await manager.JoinLobby(codeInput.text);
+    }
+
+    public void StartMatch()
+    {
+        manager.StartGame();
+    }
+}
---
+++ Assets/Scripts/Multijugador/RelayLobbyManager.cs
@@ -0,0 +1,114 @@
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+
+using UnityEngine;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+public class RelayLobbyManager : MonoBehaviour
+{
+    async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+    }
+
+    // HOST
+    public async Task<string> CreateLobby()
+    {
+        Allocation allocation =
+            await RelayService.Instance.CreateAllocationAsync(4);
+
+        string joinCode =
+            await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+        UnityTransport transport =
+            NetworkManager.Singleton.GetComponent<UnityTransport>();
+
+        transport.SetHostRelayData(
+            allocation.RelayServer.IpV4,
+            (ushort)allocation.RelayServer.Port,
+            allocation.AllocationIdBytes,
+            allocation.Key,
+            allocation.ConnectionData
+        );
+
+        CreateLobbyOptions options = new CreateLobbyOptions
+        {
+            Data = new Dictionary<string, DataObject>()
+            {
+                {
+                    "joinCode",
+                    new DataObject(
+                        visibility: DataObject.VisibilityOptions.Member,
+                        value: joinCode)
+                }
+            }
+        };
+
+        Lobby lobby =
+            await LobbyService.Instance.CreateLobbyAsync(
+                "MiLobby",
+                4,
+                options
+            );
+
+        NetworkManager.Singleton.StartHost();
+
+        Debug.Log("Lobby creado: " + lobby.LobbyCode);
+
+        return lobby.LobbyCode;
+    }
+
+    // CLIENTE
+    public async Task JoinLobby(string lobbyCode)
+    {
+        Lobby lobby =
+            await LobbyService.Instance.JoinLobbyByCodeAsync(lobbyCode);
+
+        string joinCode = lobby.Data["joinCode"].Value;
+
+        JoinAllocation allocation =
+            await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+        UnityTransport transport =
+            NetworkManager.Singleton.GetComponent<UnityTransport>();
+
+        transport.SetClientRelayData(
+            allocation.RelayServer.IpV4,
+            (ushort)allocation.RelayServer.Port,
+            allocation.AllocationIdBytes,
+            allocation.Key,
+            allocation.ConnectionData,
+            allocation.HostConnectionData
+        );
+
+        NetworkManager.Singleton.StartClient();
+
+        Debug.Log("Cliente conectado");
+    }
+
+    public void StartGame()
+    {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            NetworkManager.Singleton.SceneManager.LoadScene(
+                "Game",
+                LoadSceneMode.Single
+            );
+        }
+    }
+}
